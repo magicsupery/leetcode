f965ce167fbf28609427a9251be6100631f0eb32
杨超 <ycn2308@corp.netease.com>
2019-03-04 19:50:13 +0800

    fix problem

--- 2_ADD_TWO_NUMBERS.cpp
+++ 2_ADD_TWO_NUMBERS.cpp
@@ -12,23 +12,27 @@ class Solution {
 			int tmp_num = 0;
 			int carry = 0;
 			ListNode* res = new ListNode(0);
-
 			ListNode* pre = res;
-			while( l1 != NULL && l2 != NULL)
+
+			while( l1 != NULL || l2 != NULL)
 			{
 				tmp_num += (l1 == NULL)? 0: l1->val;
 				tmp_num += (l2 == NULL)? 0: l2->val;
 				tmp_num += carry;
 
+				carry = tmp_num / 10;
 				tmp_num %= 10;
-				carry /= 1;
 				
 				pre->next = new ListNode(tmp_num);
 				pre = pre->next;
 				
 				l1 = (l1 == NULL)? l1: l1->next;
 				l2 = (l2 == NULL)? l2: l2->next;
+				tmp_num = 0;
 			}
+			
+			if(carry == 1)
+				pre->next = new ListNode(carry);
 
 			return res->next;
 
@@ -37,17 +41,12 @@ class Solution {
 
 int main()
 {
-	ListNode* l1 = new ListNode(2);
+	ListNode* l1 = new ListNode(0);
 	auto tmp = l1;
-	tmp->next = new ListNode(4);
-	tmp = tmp->next;
-	tmp->next = new ListNode(3);
 
-	ListNode* l2 = new ListNode(5);
+	ListNode* l2 = new ListNode(1);
 	tmp = l2;
-	tmp->next = new ListNode(6);
-	tmp = tmp->next;
-	tmp->next = new ListNode(4);
+	tmp->next = new ListNode(8);
 	
 	auto res = Solution().addTwoNumbers(l1, l2);
 	cout << "res is " << res->val << endl;
